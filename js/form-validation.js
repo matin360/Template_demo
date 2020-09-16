54bbd556141f0df0ec2ab68fe4f9766a9639dd05
Матин Манафов <matin.manafovvvv@gmail.com>
2020-09-16 13:35:40 +0400

    form validation updated

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,24 +1,46 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     let inputs = ['name', 'email', 'phone', 'message', 'select'];
-    document.getElementById('submit-form').addEventListener('click', () => {
+    getElmByid('submit-form').addEventListener('click', () => {
         inputs.forEach( (inputId) => {
-            let input = document.getElementById(inputId);
-            checkInputValidty(input);
+            let input = getElmByid(inputId);
+            IsEmpty(input);
         });
     });
 
     inputs.forEach( (input) => {
-        document.getElementById(input).addEventListener('change', () => {
-            document.getElementById(input + '-lbl').innerHTML = '';
+        getElmByid(input).addEventListener('change', () => {
+            getElmByid(input.id + '-lbl').innerHTML = '';
         });
     })
 });
 
-function checkInputValidty(input) {
-    let span = document.getElementById(input.id + '-lbl');
-    if (!input.checkValidity()) {
-        span.innerHTML = input.validationMessage;
-        span.style.color = '#ff0000';
+function IsEmpty(input) {
+    let span = getElmByid(input.id + '-lbl');
+    if (input.value === '') {
+        let msg = getMessage(input.id);
+        span.innerHTML = msg;
+        getStyleToElms(span, '#ff0000', '10px');
     }
+}
+
+function getMessage(inputId) {
+    let msg = '';
+    switch (inputId) {
+        case 'name': msg = 'Please, fill out name field'; break;
+        case 'email': msg = 'Please, fill out email field'; break;
+        case 'phone': msg = 'Please, fill out phone field'; break;
+        case 'message': msg = 'Please, fill out message field'; break;
+        case 'select': msg = 'Please, fill out select field'; break;
+    }
+    return msg;
+}
+
+function getElmByid(id){
+    return document.getElementById(id);
+}
+
+function getStyleToElms(elm, color, fontSize) {
+    elm.style.color = color;
+    elm.style.fontSize = fontSize;
 }
